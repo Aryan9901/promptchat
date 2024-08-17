3255427d942ac7fabbe669f000ef7fe58a4da235
Aryan9901 <911aaryan@gmail.com>
2024-08-17 22:20:52 +0530

    updated prompt route

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -8,7 +8,16 @@ export const GET = async (request) => {
 		const prompts = await Prompt.find().populate("creator");
 		console.log(prompts);
 
-		return new Response(JSON.stringify(prompts), { status: 200 });
+		// Ensure no caching
+		return new Response(JSON.stringify(prompts), {
+			status: 200,
+			headers: {
+				"Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
+				Pragma: "no-cache",
+				Expires: "0",
+				"Surrogate-Control": "no-store",
+			},
+		});
 	} catch (error) {
 		return new Response("Internal Server Error", { status: 500 });
 	}
