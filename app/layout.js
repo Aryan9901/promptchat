5bb568cd9a30cdec430bfe3c8373869f0b5f32b8
Aryan9901 <911aaryan@gmail.com>
2024-08-17 20:59:03 +0530

    updated favicon

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,14 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 import "@styles/globals.css";
 
-export const metadata = {
-	title: "Home Page",
-	description: "Discover & share AI Prompts",
-};
-
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
+			<head>
+				<link rel="icon" href="/assets/icons/logo.ico" />
+				<meta name="description" content="Discover & share AI Prompts" />
+				<title>Promptchat - Home</title>
+			</head>
 			<body>
 				<Provider>
 					<div className="main">
